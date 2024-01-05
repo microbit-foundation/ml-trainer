1482ef76a3f1897126908e04be41f5a661e889cb
Robert Knight <robert@microbit.org>
2024-01-05 15:46:07 +0000

    Maintain common width for all connection dialogs

---
+++ src/components/DialogHeading.svelte
@@ -0,0 +1,1 @@
+<h2 class="text-2xl font-bold pb-5"><slot /></h2>
--- src/components/connection-prompt/MicrobitWearingInstructionDialog.svelte
+++ src/components/connection-prompt/MicrobitWearingInstructionDialog.svelte
@@ -9,6 +9,7 @@
   import { t } from '../../i18n.ts';
   import microbitStrapImage from '../../imgs/microbit_strap.png';
   import microbitHolderImage from '../../imgs/microbit_holder.png';
+  import DialogHeading from '../DialogHeading.svelte';
 
   export let onNextClick: () => void;
   export let onBackClick: () => void;
@@ -18,15 +19,14 @@
 </script>
 
 <main>
-  <div class="w-200">
-    <p class="font-bold text-2xl text-left leading-10">
+  <div class="w-175">
+    <DialogHeading>
       {#if currentStage === 'usb'}
         {$t('connectMB.wearingSetup.bluetooth.heading')}
       {:else if currentStage === 'usb1'}
         {$t('connectMB.wearingSetup.radioConnection.heading')}
-      {/if}
-    </p>
-    <div class="space-y-10 mt-5">
+      {/if}</DialogHeading>
+    <div class="space-y-10">
       <p class="leading-normal">
         <a class="text-link" href={instructionsVideoLink} target="_blank" rel="noopener"
           >{$t('connectMB.wearingSetup.subtitle1')}</a>
@@ -38,7 +38,7 @@
           <p>{$t('connectMB.wearingSetup.requirements1')}</p>
         </div>
         <div class="flex flex-col items-center gap-y-2">
-          <img src={microbitStrapImage} class="w-150" alt="" />
+          <img src={microbitStrapImage} class="w-175" alt="" />
           <p>{$t('connectMB.wearingSetup.requirements2')}</p>
         </div>
       </div>
