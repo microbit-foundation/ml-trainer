5c74080ff3e381cfa2dcac0b1f1d2cff0e837934
Grace <grace@microbit.org>
2024-01-05 09:52:01 +0000

    Update input outline colour to primary

--- src/components/Gesture.svelte
+++ src/components/Gesture.svelte
@@ -282,7 +282,7 @@
       >{$t('content.data.addAction.inputLabel')}</label>
     <input
       name="gestureName"
-      class="w-40 col-start-2 p-2 col-end-5 transition ease rounded bg-gray-100 placeholder-gray-500"
+      class="w-40 col-start-2 p-2 col-end-5 transition ease rounded bg-gray-100 placeholder-gray-500 outline-primary"
       contenteditable
       id="gestureName"
       placeholder={gesturePlaceholderName}
