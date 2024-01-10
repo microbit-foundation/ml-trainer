39ea5c7242cf40a71d231ac3c825c7c7eec2c293
Grace <grace@microbit.org>
2024-01-10 14:12:57 +0000

    Change loading spinner colour to secondary (green)

--- src/components/LoadingSpinner.svelte
+++ src/components/LoadingSpinner.svelte
@@ -4,11 +4,70 @@
   SPDX-License-Identifier: MIT
  -->
 
+<style>
+  /* Adapted from https://projects.lukehaas.me/css-loaders/ */
+  .loader,
+  .loader:before,
+  .loader:after {
+    border-radius: 50%;
+    width: 2.5em;
+    height: 2.5em;
+    -webkit-animation-fill-mode: both;
+    animation-fill-mode: both;
+    -webkit-animation: load7 1.8s infinite ease-in-out;
+    animation: load7 1.8s infinite ease-in-out;
+  }
+  .loader {
+    font-size: 10px;
+    margin: 80px auto;
+    position: relative;
+    text-indent: -9999em;
+    -webkit-transform: translateZ(0);
+    -ms-transform: translateZ(0);
+    transform: translateZ(0);
+    -webkit-animation-delay: -0.16s;
+    animation-delay: -0.16s;
+  }
+  .loader:before,
+  .loader:after {
+    content: '';
+    position: absolute;
+    top: 0;
+  }
+  .loader:before {
+    left: -3.5em;
+    -webkit-animation-delay: -0.32s;
+    animation-delay: -0.32s;
+  }
+  .loader:after {
+    left: 3.5em;
+  }
+  @-webkit-keyframes load7 {
+    0%,
+    80%,
+    100% {
+      box-shadow: 0 2.5em 0 -1.3em;
+    }
+    40% {
+      box-shadow: 0 2.5em 0 -1.3em;
+    }
+  }
+  @keyframes load7 {
+    0%,
+    80%,
+    100% {
+      box-shadow: 0 2.5em 0 -1.3em;
+    }
+    40% {
+      box-shadow: 0 2.5em 0 0;
+    }
+  }
+</style>
+
 <script>
-  import { t } from '../../../i18n';
-  import loadingSpinnerImage from '../imgs/loadingspinner.gif';
+  import { t } from '../i18n';
 </script>
 
 <div class="justify-center items-center self-center m-auto">
-  <img alt={$t('loading')} src={loadingSpinnerImage} width={150} />
+  <div alt={$t('loading')} class="loader text-secondary" />
 </div>
