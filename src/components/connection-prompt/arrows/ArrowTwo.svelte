d183954e18e2a2e57664ad04ae41aa61687720fb
Robert Knight <robert@microbit.org>
2024-01-11 16:15:26 +0000

    Add arrows to select micro:bit dialogs

---
+++ src/components/connection-prompt/arrows/ArrowTwo.svelte
@@ -0,0 +1,22 @@
+<!--
+  (c) 2024, Center for Computational Thinking and Design at Aarhus University and contributors
+
+  SPDX-License-Identifier: MIT
+ -->
+
+<svg
+  width="166"
+  height="180"
+  viewBox="0 0 166 180"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg">
+  <rect x="13" y="25" width="146" height="10" fill="#7BCDC2" />
+  <rect x="13" y="25" width="10" height="140" fill="#7BCDC2" />
+  <circle cx="146" cy="30" r="20" fill="#7BCDC2" />
+  <path d="M17.5 180L2.34455 143.75H32.6554L17.5 180Z" fill="#7BCDC2" />
+  <foreignObject x="126" y="10" width="40" height="40">
+    <div class="h-10 w-10 flex items-center justify-center" aria-hidden>
+      <p class="text-2xl text-white">2</p>
+    </div>
+  </foreignObject>
+</svg>
