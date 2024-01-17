c311caeee5c94e605b3876de5b4faa75cb18f935
Grace <grace@microbit.org>
2024-01-17 13:08:45 +0000

    Revert and remove main for ConnectCableDialog

--- src/components/connection-prompt/bluetooth/ConnectCableDialog.svelte
+++ src/components/connection-prompt/bluetooth/ConnectCableDialog.svelte
@@ -16,40 +16,42 @@
   export let currentStage: string;
 </script>
 
-<div class="w-175">
-  <DialogHeading>
-    {#if currentStage === 'usb'}
-      {$t('connectMB.connectCable.heading')}
-    {:else if currentStage === 'usb1'}
-      {$t('connectMB.connectCableMB1.heading')}
-    {:else if currentStage === 'usb2'}
-      {$t('connectMB.connectCableMB2.heading')}
-    {/if}
-  </DialogHeading>
-  <div class="space-y-5">
-    <p>
+<main>
+  <div class="w-175">
+    <DialogHeading>
       {#if currentStage === 'usb'}
-        {$t('connectMB.connectCable.subtitle')}
+        {$t('connectMB.connectCable.heading')}
       {:else if currentStage === 'usb1'}
-        {$t('connectMB.connectCableMB1.subtitle')}
+        {$t('connectMB.connectCableMB1.heading')}
       {:else if currentStage === 'usb2'}
-        {$t('connectMB.connectCableMB2.subtitle')}
+        {$t('connectMB.connectCableMB2.heading')}
       {/if}
-    </p>
-    <img
-      src={connectCableImage}
-      alt={$t('connectMB.connectCable.altText')}
-      class="w-65 h-241px mx-auto" />
+    </DialogHeading>
+    <div class="space-y-5">
+      <p>
+        {#if currentStage === 'usb'}
+          {$t('connectMB.connectCable.subtitle')}
+        {:else if currentStage === 'usb1'}
+          {$t('connectMB.connectCableMB1.subtitle')}
+        {:else if currentStage === 'usb2'}
+          {$t('connectMB.connectCableMB2.subtitle')}
+        {/if}
+      </p>
+      <img
+        src={connectCableImage}
+        alt={$t('connectMB.connectCable.altText')}
+        class="w-65 h-241px mx-auto" />
+    </div>
   </div>
-</div>
-<div class="flex justify-between pt-5">
-  {#if currentStage === 'usb' || currentStage === 'usb1'}
-    <StandardButton type="link" onClick={onSkipClick}
-      >{$t('connectMB.connectCable.skip')}</StandardButton>
-  {/if}
-  <div class="flex gap-x-5 ml-auto">
-    <StandardButton onClick={onBackClick}>{$t('connectMB.backButton')}</StandardButton>
-    <StandardButton type="primary" onClick={onNextClick}
-      >{$t('connectMB.nextButton')}</StandardButton>
+  <div class="flex justify-between pt-5">
+    {#if currentStage === 'usb' || currentStage === 'usb1'}
+      <StandardButton type="link" onClick={onSkipClick}
+        >{$t('connectMB.connectCable.skip')}</StandardButton>
+    {/if}
+    <div class="flex gap-x-5 ml-auto">
+      <StandardButton onClick={onBackClick}>{$t('connectMB.backButton')}</StandardButton>
+      <StandardButton type="primary" onClick={onNextClick}
+        >{$t('connectMB.nextButton')}</StandardButton>
+    </div>
   </div>
-</div>
+</main>
