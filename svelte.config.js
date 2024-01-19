c1c08ff23d5380a90cae72d6071afae1ad608841
Grace <grace@microbit.org>
2024-01-19 15:03:29 +0000

    Install melt-ui and preprocessor

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,11 @@
  *
  * SPDX-License-Identifier: MIT
  */
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { preprocessMeltUI, sequence } from '@melt-ui/pp';
 
 export default {
-	// Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-	// for more information about preprocessors
-	preprocess: vitePreprocess()
+  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
+  // for more information about preprocessors
+  preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
 };
