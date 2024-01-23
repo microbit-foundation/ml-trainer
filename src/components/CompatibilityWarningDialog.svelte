d064aaf6d5e876aa5f9229a731c11858282c97a5
Grace <grace@microbit.org>
2024-01-23 09:17:09 +0000

    Use melt dialog in compatibility warning dialog

--- src/components/CompatibilityWarningDialog.svelte
+++ src/components/CompatibilityWarningDialog.svelte
@@ -6,30 +6,28 @@
 
 <script lang="ts">
   import { t } from '../i18n';
-  import StandardDialog from './dialogs/StandardDialog.svelte';
+  import MeltDialog from './dialogs/MeltDialog.svelte';
   import StandardButton from './StandardButton.svelte';
   import { isCompatibilityWarningDialogOpen } from '../script/stores/uiStore';
-  import DialogHeading from './DialogHeading.svelte';
 
   const onClose = () => {
     $isCompatibilityWarningDialogOpen = false;
   };
 </script>
 
-<StandardDialog isOpen={$isCompatibilityWarningDialogOpen} {onClose}>
-  <div class="w-175">
-    <DialogHeading>
-      {$t('popup.compatibility.header')}
-    </DialogHeading>
-    <div class="space-y-5">
-      <div class="space-y-2">
-        <p>{$t('popup.compatibility.explain')}</p>
-        <p>{$t('popup.compatibility.advice')}</p>
-      </div>
-      <div class="flex justify-end">
-        <StandardButton onClick={onClose} type="primary"
-          >{$t('actions.close')}</StandardButton>
-      </div>
+<MeltDialog
+  isOpen={$isCompatibilityWarningDialogOpen}
+  {onClose}
+  class="w-175"
+  titleId="popup.compatibility.header">
+  <div class="space-y-5">
+    <div class="space-y-2">
+      <p>{$t('popup.compatibility.explain')}</p>
+      <p>{$t('popup.compatibility.advice')}</p>
+    </div>
+    <div class="flex justify-end">
+      <StandardButton onClick={onClose} type="primary"
+        >{$t('actions.close')}</StandardButton>
     </div>
   </div>
-</StandardDialog>
+</MeltDialog>
