a304bf464a125ba6dc4bf2e5ad8d2b45ee6e66cb
Grace <grace@microbit.org>
2024-01-24 17:25:15 +0000

    Reconnect button restarts connection flow

--- src/components/ReconnectPrompt.svelte
+++ src/components/ReconnectPrompt.svelte
@@ -8,11 +8,9 @@
   import StandardButton from '../components/StandardButton.svelte';
   import { state } from '../script/stores/uiStore';
   import { t } from '../i18n';
-  import { btPatternInput, btPatternOutput } from '../script/stores/connectionStore';
-  import MBSpecs from '../script/microbit-interfacing/MBSpecs';
-  import Microbits from '../script/microbit-interfacing/Microbits';
   import { DeviceRequestStates } from '../script/stores/connectDialogStore';
   import StandardDialog from './dialogs/StandardDialog.svelte';
+  import { startConnectionProcess } from '../script/stores/connectDialogStore';
 
   export let isOpen: boolean = false;
 
@@ -27,40 +25,19 @@
           buttonId: 'disconnectedWarning.reconnectButton.output',
         };
 
-  // When disconnected by lost connection, offer the option to attempt to reconnect
-  let hideReconnectMessageAfterTimeout = false;
-  state.subscribe(s => {
-    if (s.offerReconnect) {
-      hideReconnectMessageAfterTimeout = true;
-    }
-  });
-
-  const reconnect = (connectState: DeviceRequestStates) => {
-    hideReconnectMessageAfterTimeout = false;
-    console.assert(connectState != DeviceRequestStates.NONE);
-    const pairingPattern =
-      connectState === DeviceRequestStates.INPUT ? $btPatternInput : $btPatternOutput;
-    const name = MBSpecs.Utility.patternToName(pairingPattern);
-
-    const connect = () => {
-      if (connectState == DeviceRequestStates.INPUT) {
-        return Microbits.assignBluetoothInput(name);
-      }
-      return Microbits.assignOutput(name);
-    };
-
-    void connect().then(didSucceed => {
-      if (didSucceed) {
-        $state.offerReconnect = false;
-      }
-    });
+  const stopOfferingReconnect = () => {
+    $state.offerReconnect = false;
+  };
+  const reconnect = () => {
+    startConnectionProcess();
+    stopOfferingReconnect();
   };
 </script>
 
-<StandardDialog {isOpen} onClose={() => ($state.offerReconnect = false)} class="w-100">
+<StandardDialog {isOpen} onClose={stopOfferingReconnect} class="w-100">
   <p>{$t(dialogText.bodyId)}</p>
   <div class="flex justify-center">
-    <StandardButton type="primary" onClick={() => reconnect($state.reconnectState)}
+    <StandardButton type="primary" onClick={reconnect}
       >{$t(dialogText.buttonId)}</StandardButton>
   </div>
 </StandardDialog>
