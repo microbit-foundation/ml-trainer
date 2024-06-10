cf8c8bd193689f99edc8b3b91d7708480b43bf40
Grace <grace@microbit.org>
2024-06-10 17:32:39 +0100

    Use LED pattern for record countdown

--- src/components/output/SimpleLedMatrix.svelte
+++ src/components/output/SimpleLedMatrix.svelte
@@ -21,9 +21,10 @@
 
 <script lang="ts">
   export let matrix: boolean[];
+  export let ariaLabel: string | undefined = undefined;
 </script>
 
-<div class="buttonGrid h-18 w-18 select-none ml-0">
+<div class="buttonGrid h-18 w-18 select-none ml-0" role="img" aria-label={ariaLabel}>
   <!-- Draw all 25 boxes -->
   {#each matrix as button}
     <div
