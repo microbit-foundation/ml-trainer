b62a2632a42496d8b87a26a463573a5df08cfbaf
Grace <grace@microbit.org>
2024-06-18 16:56:40 +0100

    Revert "Generate and download mkcd file"
    This reverts commit 75c02dc4c2d2f81db3473b379fa47f3dede48f23.

--- src/lzma.d.ts
+++ src/lzma.d.ts
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 /**
  * (c) 2023, Center for Computational Thinking and Design at Aarhus University and contributors
  *
  * SPDX-License-Identifier: MIT
  */
 
-=======
->>>>>>> 7b95b33 (Generate and download mkcd file)
 declare module 'lzma/src/lzma_worker';
