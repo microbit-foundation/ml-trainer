36d0faa3c07d7c5d1973ab67148fadb39b454a7e
Grace <grace@microbit.org>
2024-07-04 16:02:35 +0100

    Tweak manual flashing dialog
    - make sure images are correct aspect ratio
    - add space between next btn and gif

--- src/components/ManualFlashingDialog.tsx
+++ src/components/ManualFlashingDialog.tsx
@@ -1,6 +1,6 @@
 import { Image, Text, VStack } from "@chakra-ui/react";
 import Bowser from "bowser";
-import { ReactNode, useCallback } from "react";
+import { ReactNode, useCallback, useEffect } from "react";
 import { FormattedMessage, useIntl } from "react-intl";
 import transferProgramChromeOS from "../images/transfer_program_chromeos.gif";
 import transferProgramMacOS from "../images/transfer_program_macos.gif";
@@ -13,20 +13,20 @@ import { ConnType } from "../connection-flow";
 
 interface ImageProps {
   src: string;
-  height: string;
+  height: number;
 }
 
 // See https://github.com/lancedikson/bowser/blob/master/src/constants.js
 const getImageProps = (os: string): ImageProps => {
   switch (os) {
     case "Chrome OS":
-      return { src: transferProgramChromeOS, height: "324px" };
+      return { src: transferProgramChromeOS, height: 300 };
     case "Windows":
-      return { src: transferProgramWindows, height: "390px" };
+      return { src: transferProgramWindows, height: 362 };
     case "macOS":
-      return { src: transferProgramMacOS, height: "387px" };
+      return { src: transferProgramMacOS, height: 360 };
     default:
-      return { src: transferProgramWindows, height: "390px" };
+      return { src: transferProgramWindows, height: 392 };
   }
 };
 
@@ -56,6 +56,10 @@ const ManualFlashingDialog = ({ ...props }: ManualFlashingDialogProps) => {
     );
   }, []);
 
+  useEffect(() => {
+    handleDownload();
+  }, [handleDownload]);
+
   return (
     <ConnectContainerDialog
       headingId="connectMB.transferHex.heading"
@@ -82,6 +86,7 @@ const ManualFlashingDialog = ({ ...props }: ManualFlashingDialogProps) => {
           height={imageProps.height}
           src={imageProps.src}
           alt={intl.formatMessage({ id: "connectMB.transferHex.altText" })}
+          mb={5}
         />
       </VStack>
     </ConnectContainerDialog>
