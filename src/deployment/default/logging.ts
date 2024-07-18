ca9583054488b6e8842190e705c10cbf03fdef1a
Matt Hillsdon <matt.hillsdon@microbit.org>
2024-07-18 17:34:02 +0100

    Revert temporary changes; upgrade lib

--- src/deployment/default/logging.ts
+++ src/deployment/default/logging.ts
@@ -7,14 +7,7 @@
 import { Logging } from "../../logging/logging";
 
 export class NullLogging implements Logging {
-  event(e: any): void {
-    console.log(e);
-  }
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  error(e: any): void {
-    console.error(e);
-  }
-  log(e: any): void {
-    console.log(e);
-  }
+  event(): void {}
+  error(): void {}
+  log(): void {}
 }
