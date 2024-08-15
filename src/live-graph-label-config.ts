58a4462437753073d240a80ad9b84dc5af83714b
Grace <grace@microbit.org>
2024-08-15 17:21:32 +0100

    Tweak to make more concise

--- src/live-graph-label-config.ts
+++ src/live-graph-label-config.ts
@@ -17,30 +17,27 @@ export const getUpdatedLabelConfig = (
     ...config,
     arrowHeight: getArrowHeight(dataPoint[config.label]),
   }));
-  const fixedLabels = fixOverlappingLabels(newLabelConfigs);
-  return fixedLabels ?? newLabelConfigs;
+  return fixOverlappingLabels(newLabelConfigs);
 };
 
 const getArrowHeight = (pos: number) => (2.1 - pos) * 2.32;
 
-const fixOverlappingLabels = (labels: LabelConfig[]) => {
-  labels.sort((a, b) => {
-    return a.arrowHeight - b.arrowHeight;
-  });
+const fixOverlappingLabels = (labels: LabelConfig[]): LabelConfig[] => {
+  labels.sort((a, b) => a.arrowHeight - b.arrowHeight);
 
   const height0 = labels[0].arrowHeight;
   const height1 = labels[1].arrowHeight;
   const height2 = labels[2].arrowHeight;
 
-  const maxDistanceBetweenAll = height2 - height0;
+  const currMaxDistanceBetweenAll = height2 - height0;
 
   // If all the labels are too close, we find the middle and position the labels around it.
-  if (maxDistanceBetweenAll < maxDistance * 2) {
-    const middle = maxDistanceBetweenAll / 2 + height0;
-    labels[0].labelHeight = middle - maxDistance;
-    labels[1].labelHeight = middle;
-    labels[2].labelHeight = middle + maxDistance;
-    return;
+  if (currMaxDistanceBetweenAll < maxDistance * 2) {
+    const midArrowHeight = currMaxDistanceBetweenAll / 2 + height0;
+    labels[0].labelHeight = midArrowHeight - maxDistance;
+    labels[1].labelHeight = midArrowHeight;
+    labels[2].labelHeight = midArrowHeight + maxDistance;
+    return labels;
   }
 
   labels[0].labelHeight = height0;
@@ -52,9 +49,9 @@ const fixOverlappingLabels = (labels: LabelConfig[]) => {
     const diff = labels[i + 1].labelHeight - labels[i].labelHeight;
     if (diff > maxDistance) continue;
 
-    const middle = diff / 2 + labels[i].labelHeight;
-    labels[i + 1].labelHeight = middle + maxDistance / 2;
-    labels[i].labelHeight = middle - maxDistance / 2;
+    const midArrowHeight = diff / 2 + labels[i].labelHeight;
+    labels[i + 1].labelHeight = midArrowHeight + maxDistance / 2;
+    labels[i].labelHeight = midArrowHeight - maxDistance / 2;
 
     // Only one of the labels will be close to the other, otherwise all are too close.
     break;
