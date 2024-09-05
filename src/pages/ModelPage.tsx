39597f16444765cdb47f87ca8f38cfffeb7f4d37
Grace <grace@microbit.org>
2024-09-05 12:10:25 +0100

    Redirect to test model page when not trained

--- src/pages/ModelPage.tsx
+++ src/pages/ModelPage.tsx
@@ -1,11 +1,10 @@
 import { Button } from "@chakra-ui/react";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 import { FormattedMessage } from "react-intl";
 import { useNavigate } from "react-router";
 import BackArrow from "../components/BackArrow";
 import DefaultPageLayout from "../components/DefaultPageLayout";
 import ModelGridView from "../components/ModelGridView";
-import TrainModelFirstView from "../components/TrainModelFirstView";
 import { MlStage, useMlStatus } from "../ml-status-hooks";
 import { SessionPageId } from "../pages-config";
 import { createSessionPageUrl } from "../urls";
@@ -13,10 +12,17 @@ import { createSessionPageUrl } from "../urls";
 const ModelPage = () => {
   const navigate = useNavigate();
   const [{ stage }] = useMlStatus();
-  const handleOnBack = useCallback(() => {
+
+  const navigateToDataSamples = useCallback(() => {
     navigate(createSessionPageUrl(SessionPageId.DataSamples));
   }, [navigate]);
-  return (
+
+  useEffect(() => {
+    if (stage !== MlStage.TrainingComplete) {
+      navigateToDataSamples();
+    }
+  });
+  return stage === MlStage.TrainingComplete ? (
     <DefaultPageLayout
       titleId={`${SessionPageId.Model}-title`}
       showPageTitle
@@ -26,7 +32,7 @@ const ModelPage = () => {
           leftIcon={<BackArrow color="white" />}
           variant="plain"
           color="white"
-          onClick={handleOnBack}
+          onClick={navigateToDataSamples}
           pr={3}
           pl={3}
         >
@@ -34,12 +40,10 @@ const ModelPage = () => {
         </Button>
       }
     >
-      {stage === MlStage.TrainingComplete ? (
-        <ModelGridView />
-      ) : (
-        <TrainModelFirstView />
-      )}
+      <ModelGridView />
     </DefaultPageLayout>
+  ) : (
+    <></>
   );
 };
 
