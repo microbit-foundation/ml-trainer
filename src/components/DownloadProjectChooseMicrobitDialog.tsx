1ea8dd5d211591d81d505df08b6be8a45ff19c95
Grace <grace@microbit.org>
2024-09-06 13:54:36 +0100

    WIP Choose micro:bit dialog

--- src/components/DownloadProjectChooseMicrobitDialog.tsx
+++ src/components/DownloadProjectChooseMicrobitDialog.tsx
@@ -1,4 +1,14 @@
-import { HStack, Image, Text, VStack } from "@chakra-ui/react";
+import {
+  Box,
+  HStack,
+  Icon,
+  Image,
+  Text,
+  useRadio,
+  UseRadioProps,
+  VStack,
+} from "@chakra-ui/react";
+import { RiCheckboxBlankLine } from "react-icons/ri";
 import { FormattedMessage } from "react-intl";
 import microbitImage from "../images/stylised-microbit-black.svg";
 import twoMicrobitsImage from "../images/stylised-two-microbits-black.svg";
@@ -26,13 +36,13 @@ const DownloadProjectChooseMicrobitDialog = ({
         <Text textAlign="left" w="full">
           <FormattedMessage id="download-project-choose-microbit-subtitle" />
         </Text>
-        <HStack gap={5}>
-          <Option
+        <HStack gap={5} w="full" justifyContent="space-evenly">
+          <RadioCard
             titleId="download-project-same-microbit-option"
             onClick={onSameMicrobitClick}
             imgSrc={microbitImage}
           />
-          <Option
+          <RadioCard
             titleId="download-project-another-microbit-option"
             onClick={onDifferentMicrobitClick}
             imgSrc={twoMicrobitsImage}
@@ -43,35 +53,55 @@ const DownloadProjectChooseMicrobitDialog = ({
   );
 };
 
-interface OptionProps {
+interface RadioCardProps extends UseRadioProps {
+  // children: ReactNode;
   onClick: () => void;
   titleId: string;
   imgSrc: string;
 }
 
-const Option = ({ onClick, titleId, imgSrc }: OptionProps) => {
+const RadioCard = ({ titleId, imgSrc, ...props }: RadioCardProps) => {
+  const { getInputProps, getRadioProps } = useRadio(props);
+
+  const input = getInputProps();
+  const checkbox = getRadioProps();
+
   return (
-    <VStack
-      bgColor="white"
-      borderRadius="5px"
-      overflow="hidden"
-      maxW="450px"
-      w="50%"
-      borderStyle="solid"
-      borderColor="gray.200"
-      borderWidth="5px"
-      _hover={{ bgColor: "gray.200" }}
-      transition="background-color 0.25s"
-      p={3}
-      gap={3}
-      alignItems="stretch"
-      onClick={onClick}
-    >
-      <Text fontWeight="bold" fontSize="md" w="full" textAlign="center">
-        <FormattedMessage id={titleId} />
-      </Text>
-      <Image src={imgSrc} alt="" htmlWidth="400px" px={10} py={3} />
-    </VStack>
+    <Box as="label">
+      <input {...input} />
+      <Box
+        {...checkbox}
+        cursor="pointer"
+        borderWidth="2px"
+        borderRadius="md"
+        _checked={{
+          borderWidth: "5px",
+          borderColor: "brand.600",
+        }}
+        _focus={{
+          boxShadow: "outline",
+        }}
+        p={4}
+      >
+        <HStack justifyContent="right" mb={-8}>
+          <Icon as={RiCheckboxBlankLine} boxSize={6} color="brand.600" />
+        </HStack>
+        <VStack>
+          <Image
+            src={imgSrc}
+            alt=""
+            htmlWidth="220px"
+            px={12}
+            py={3}
+            position="relative"
+            justifySelf="center"
+          />
+          <Text fontWeight="bold" fontSize="md" w="full">
+            <FormattedMessage id={titleId} />
+          </Text>
+        </VStack>
+      </Box>
+    </Box>
   );
 };
 
