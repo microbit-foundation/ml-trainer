bb000265bf741efaeba7951db84fc06541aadc82
Matt Hillsdon <matt.hillsdon@microbit.org>
2024-09-17 11:06:59 +0100

    Deduplicate

--- src/components/UploadDataSamplesMenuItem.tsx
+++ src/components/UploadDataSamplesMenuItem.tsx
@@ -2,13 +2,10 @@ import { Input, MenuItem } from "@chakra-ui/react";
 import { useCallback, useRef } from "react";
 import { RiUpload2Line } from "react-icons/ri";
 import { FormattedMessage } from "react-intl";
-import { isDatasetUserFileFormat } from "../model";
-import { useAppStore } from "../store";
-import { readFileAsText } from "../utils/fs-util";
+import { useProject } from "../hooks/project-hooks";
 
 const UploadDataSamplesMenuItem = () => {
-  const loadDataset = useAppStore((s) => s.loadDataset);
-
+  const { loadProject } = useProject();
   const inputRef = useRef<HTMLInputElement>(null);
 
   const handleChooseFile = useCallback(() => {
@@ -16,30 +13,23 @@ const UploadDataSamplesMenuItem = () => {
   }, []);
 
   const onOpen = useCallback(
-    async (files: File[]) => {
-      if (files.length === 0) {
-        throw new Error("Expected to be called with at least one file");
-      }
-      const gestureData = await readFileAsText(files[0]);
-      const json = JSON.parse(gestureData) as unknown;
-      if (isDatasetUserFileFormat(json)) {
-        loadDataset(json);
-      } else {
-        // TODO: notify user, also JSON parse error
+    (files: File[]) => {
+      if (files.length === 1) {
+        loadProject(files[0]);
       }
     },
-    [loadDataset]
+    [loadProject]
   );
 
   const handleOpenFile = useCallback(
-    async (e: React.ChangeEvent<HTMLInputElement>) => {
+    (e: React.ChangeEvent<HTMLInputElement>) => {
       const files = e.target.files;
       if (files) {
         const filesArray = Array.from(files);
         // Clear the input so we're triggered if the user opens the same file again.
         inputRef.current!.value = "";
         if (filesArray.length > 0) {
-          await onOpen(filesArray);
+          onOpen(filesArray);
         }
       }
     },
