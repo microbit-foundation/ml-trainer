e7f1ed479a7c7cc1fa5f18fd8d97e6abb004236a
Matt Hillsdon <matt.hillsdon@microbit.org>
2024-11-05 12:30:18 +0000

    Version using brand palette

--- src/hooks/use-graph-colors.ts
+++ src/hooks/use-graph-colors.ts
@@ -1,9 +1,8 @@
 import { useToken } from "@chakra-ui/react";
 
 export const useGraphColors = () => {
-  // colorblind wheel from https://seaborn.pydata.org/tutorial/color_palettes.html
-  const x = useToken("colors", "rgb(213, 94, 0)");
-  const y = useToken("colors", "rgb(1, 158, 115)");
-  const z = useToken("colors", "rgb(87, 180, 233)");
+  const x = useToken("colors", "red.300");
+  const y = useToken("colors", "brand2.300");
+  const z = useToken("colors", "brand.600");
   return { x, y, z };
 };
