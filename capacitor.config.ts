55e8cc78b4e871fe33df2622b2e640d42e1a197a
Grace <grace@microbit.org>
2025-01-14 13:16:49 +0000

    Initial capacitor integration config

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ml.trainer.grace',
+  appName: 'ml-trainer',
+  webDir: 'dist'
+};
+
+export default config;
