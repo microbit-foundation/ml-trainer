8f3b9d7469e863878914ab30277e030022c00e90
Grace <grace@microbit.org>
2025-06-24 10:16:47 +0100

    Workaround to avoiding button loading dom changes when doing in-context translations

---
+++ src/components/ButtonWithLoading.tsx
@@ -0,0 +1,7 @@
+import { Button, ButtonProps } from "@chakra-ui/react"
+import { flags } from "../flags"
+
+// Workaround to avoid error caused by DOM changes when doing in-context translations.
+export const ButtonWithLoading = ({isLoading, ...props}: ButtonProps) => {
+    return <Button {...props} isLoading={isLoading && !flags.translate} disabled={isLoading}/>
+}
