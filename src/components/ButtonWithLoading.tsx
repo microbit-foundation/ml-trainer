20fd7ffbb6c4928af3a0b0bb697a7173a890d7eb
Matt Hillsdon <matt.hillsdon@microbit.org>
2025-06-25 16:10:01 +0100

    Move comment for clarity

--- src/components/ButtonWithLoading.tsx
+++ src/components/ButtonWithLoading.tsx
@@ -1,11 +1,11 @@
 import { Button, ButtonProps } from "@chakra-ui/react";
 import { flags } from "../flags";
 
-// Workaround to avoid error caused by DOM changes when doing in-context translations.
 export const ButtonWithLoading = ({ isLoading, ...props }: ButtonProps) => {
   return (
     <Button
       {...props}
+      // Workaround to avoid error caused by DOM changes when doing in-context translations.
       isLoading={isLoading && !flags.translate}
       disabled={isLoading}
     />
