d376fabf07076a5ac6934044dec2e9512d44799b
Grace <grace@microbit.org>
2025-08-27 12:48:40 +0100

    Revert "Revert bundling of ML extension"
    This reverts commit 22a09a27f54fae5cbf4d1cd7e2351c96bc0ca127.

--- poc-capacitor-build.sh
+++ poc-capacitor-build.sh
@@ -14,6 +14,38 @@ if [ ! -d "$pxt_microbit_build_dir" ] || [ -z "$(ls -A "$pxt_microbit_build_dir"
     cd -
 fi
 
+# Copy static packaged pxt-microbit into built ml-trainer.
+echo "Copying pxt_microbit_build_dir into $output_dir..."
+cp -r "$pxt_microbit_build_dir" "$output_dir"
+
+# Generate and add pxt-microbit-ml extension into static package such that the path is the same.
+cd "$output_dir/$path_staticpkg_prefix"
+ext_urls=(
+    "https://makecode.com/api/gh/microbit-foundation/pxt-microbit-ml/v1.0.11/text"
+    "https://makecode.com/api/gh/microbit-foundation/pxt-microbit-ml-runner/v0.4.8/text"
+)
+
+# Process each URL in one loop
+for ext_url in "${ext_urls[@]}"; do
+    # Extract the path after the base URL
+    url_path=$(echo "$ext_url" | sed 's|https://makecode.com/api/||')
+    
+    # Generate output directory and filename from URL
+    ext_output_dir="./$(dirname "$url_path")"
+    filename="$(basename "$url_path").json"
+    ext_output_file="$ext_output_dir/$filename"
+    
+    # Create the output dir if it doesn't exist
+    if [ ! -d "$ext_output_dir" ]; then
+        echo "Creating directory: $ext_output_dir"
+        mkdir -p "$ext_output_dir"
+    fi
+    
+    echo "Fetching data from: $ext_url"
+    echo "Saving to: $ext_output_file"
+    curl -Ls "$ext_url" -o "$ext_output_file"
+done
+
 echo "Done"
 
 # To serve locally to check.
