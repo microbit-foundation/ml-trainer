a9f10d01e673838e77652b7d70b5c4494e2d05bb
Matt Hillsdon <matt.hillsdon@microbit.org>
2025-10-16 17:39:48 +0100

    My failure to provoke the same failure in e2e

--- src/e2e/import-project.spec.ts
+++ src/e2e/import-project.spec.ts
@@ -3,6 +3,9 @@
  *
  * SPDX-License-Identifier: MIT
  */
+import { Page } from "playwright/test";
+import { DataSamplesPage } from "./app/data-samples";
+import { ImportPage } from "./app/import-page";
 import { test } from "./fixtures";
 
 test.describe("import project (microbit.org case)", () => {
@@ -19,4 +22,55 @@ test.describe("import project (microbit.org case)", () => {
     await dataSamplesPage.expectOnPage();
     await dataSamplesPage.expectActions(["exercising", "not exercising"]);
   });
+
+  test("cross-tab behavior regression", async ({
+    newPage: newPage1,
+    dataSamplesPage: dataSamplesPage1,
+    context,
+  }) => {
+    // https://github.com/microbit-foundation/ml-trainer/issues/618
+    await newPage1.goto();
+    await newPage1.startNewSession();
+    await dataSamplesPage1.expectOnPage();
+
+    const page2 = await context.newPage();
+    const importPage2 = new ImportPage(page2);
+    await importPage2.gotoSimpleAIExerciseTimer();
+    await importPage2.startSession();
+    const dataSamplesPage2 = new DataSamplesPage(page2);
+    await dataSamplesPage2.expectOnPage();
+    await dataSamplesPage2.expectActions(["exercising", "not exercising"]);
+
+    await triggerVisibilityChanged(page2, true);
+    await triggerVisibilityChanged(page2, false);
+
+    await dataSamplesPage2.expectActions([]);
+  });
 });
+
+const triggerVisibilityChanged = async (page: Page, hidden: boolean) => {
+  const frames = page.frames();
+  if (frames.length > 0) {
+    await Promise.allSettled(
+      page.frames().map((frame) => frame.waitForLoadState("networkidle"))
+    );
+  }
+
+  const makecode = frames.find((f) => {
+    return (
+      // Allow for fragments, extra params.
+      f.url().startsWith("https://makecode.microbit.org/?lang=en&controller=2")
+    );
+  });
+
+  const setHidden = (value: boolean) => {
+    Object.defineProperty(document, "visibilityState", {
+      value: value ? "hidden" : "visible",
+      writable: true,
+    });
+    Object.defineProperty(document, "hidden", { value, writable: true });
+    document.dispatchEvent(new Event("visibilitychange", { bubbles: true }));
+  };
+  await makecode!.evaluate(setHidden, hidden);
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+};
