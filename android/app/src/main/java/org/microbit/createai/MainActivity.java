93570812120a04a8bf77661a71bf068d781addf8
Matt Hillsdon <matt.hillsdon@microbit.org>
2025-11-26 15:03:39 +0000

    Initial app generation (no customisation yet)

---
+++ android/app/src/main/java/org/microbit/createai/MainActivity.java
@@ -0,0 +1,5 @@
+package org.microbit.createai;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.microbit.createai',
+  appName: 'micro:bit CreateAI',
+  webDir: 'dist'
+};
+
+export default config;
