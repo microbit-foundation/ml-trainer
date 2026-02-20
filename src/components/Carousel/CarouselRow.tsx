6018976bedf79c14c9e1c1d24643e65aca18b3e1
Matt Hillsdon <matt.hillsdon@microbit.org>
2026-02-20 17:15:19 +0000

    Visual tweaks mainly motivated by mobile layout

--- src/components/Carousel/CarouselRow.tsx
+++ src/components/Carousel/CarouselRow.tsx
@@ -20,7 +20,13 @@ const CarouselRow = ({
 }: CarouselRowProps) => {
   return (
     <Box w="100%" py={8}>
-      <HStack px={[3, null, 16]} mt={2} mb={2} spacing={12}>
+      <HStack
+        px={[3, null, 16]}
+        mt={2}
+        mb={2}
+        spacing={{ base: 3, sm: 12 }}
+        justifyContent={{ base: "space-between", sm: "flex-start" }}
+      >
         {typeof titleId === "string" ? (
           <Heading as="h2" size="lg">
             <FormattedMessage id={titleId} />
