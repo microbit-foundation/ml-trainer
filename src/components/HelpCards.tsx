e3ce5d298c16763a8bc2bd9dd8609628aa3c8e14
Grace <145345672+microbit-grace@users.noreply.github.com>
2026-03-19 13:56:14 +0000

    Add accessibility card and update help card images (#790)

--- src/components/HelpCards.tsx
+++ src/components/HelpCards.tsx
@@ -1,7 +1,12 @@
 import ResourceCard from "./ResourceCard";
-import helpImage1 from "theme-package/images/user-guide.jpg";
-import helpImage2 from "theme-package/images/troubleshooting.png";
-import { supportSiteFolderUrl, userGuideUrl } from "../utils/external-links";
+import helpImage1 from "theme-package/images/user-guide.svg";
+import helpImage2 from "theme-package/images/troubleshooting.svg";
+import helpImage3 from "theme-package/images/accessibility.svg";
+import {
+  accessibilityUrl,
+  supportSiteFolderUrl,
+  userGuideUrl,
+} from "../utils/external-links";
 import { IntlShape } from "react-intl";
 
 interface HelpConfig {
@@ -22,6 +27,11 @@ const helpConfig: HelpConfig[] = [
     titleId: "troubleshooting-resource-title",
     url: supportSiteFolderUrl(),
     imgSrc: helpImage2,
+  },
+  {
+    titleId: "Accessibility",
+    url: accessibilityUrl(),
+    imgSrc: helpImage3,
   },
 ];
 
