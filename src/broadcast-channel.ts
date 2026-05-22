45bf7ec905b6b369a0c3231c38774768e941d560
Grace <grace@microbit.org>
2026-05-22 16:27:03 +0100

    Add broadcast channel shim to fix iOS <15.4 crash

--- src/broadcast-channel.ts
+++ src/broadcast-channel.ts
@@ -9,6 +9,19 @@ export interface BroadcastChannelData {
   projectIds: string[];
   settings?: boolean;
 }
+class NoOpBroadcastChannel {
+  constructor() {}
+  postMessage(_data: unknown) {}
+  addEventListener(_type: string, _handler: (e: MessageEvent) => void) {}
+  removeEventListener(_type: string, _handler: (e: MessageEvent) => void) {}
+  close() {}
+}
 
 // Used to keep project state synced between open tabs / windows.
-export const broadcastChannel = new BroadcastChannel("ml");
+// Broadcast channel is not supported for older browsers/iOS versions, so a
+// no-op version is used to avoid throwing an error.
+// TODO: Revisit making project state syncing work for older web browsers.
+export const broadcastChannel =
+  typeof BroadcastChannel === "undefined"
+    ? new NoOpBroadcastChannel()
+    : new BroadcastChannel("ml");
